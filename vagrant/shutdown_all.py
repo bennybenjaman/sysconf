c21d2e659419ad7c9984fdc058825b1a1fc84aa5
Giampaolo Rodola <g.rodola@gmail.com>
2016-10-12 15:56:02 +0200

    add script to shut down all running VMs

---
+++ vagrant/shutdown_all.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+"""
+Gracefully shuts down all running VMs.
+"""
+
+import contextlib
+import os
+import sys
+
+
+@contextlib.contextmanager
+def cwd(path):
+    cur_dir = os.getcwd()
+    try:
+        os.chdir(path)
+        yield
+    finally:
+        os.chdir(cur_dir)
+
+
+def main():
+    for name in os.listdir('.'):
+        if os.path.isdir(name):
+            if os.path.isfile(os.path.join(name, 'Vagrantfile')):
+                with cwd(name):
+                    print("shutdown %s" % name)
+                    ret = os.system('vagrant halt')
+                    if ret != 0:
+                        sys.exit(ret)
+
+
+if __name__ == '__main__':
+    main()
